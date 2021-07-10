d382257dcbcfb5843fbc53e2ca8be2aceb82ed7a
DakshAgarwal2008 <77101117+DakshAgarwal2008@users.noreply.github.com>
2021-07-10 14:27:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var gameState=0;
+var playerCount;
+var form,player,game;
+var database;
+var allPlayers;
+
+function setup(){
+    database=firebase.database();
+
+    createCanvas(500,500);
+    game=new Game();
+    game.getState();
+    game.start();                                                   
+}
+function draw(){
+if(playerCount===4){
+    game.update(1);
+}           
+if (gameState===1){
+    game.play();
+}
+
+
+}
+    
